74cc04eca0f976779d76087629cfecdc56cb6f1e
patison5 <patison4@yandex.ru>
2019-09-22 02:43:13 +0300

    add search form

--- index.php
+++ index.php
@@ -8,7 +8,10 @@
 	<title>Document</title>
 
 	<link rel="stylesheet" href="css/style.css">
-	<script src="js/app.js"></script>
+
+	<script type="text/javascript" src="js/jquery-latest.js"></script>
+	<script type="text/javascript" src="js/jquery.tablesorter.js"></script>
+	<!-- <script src="js/app.js"></script> -->
 </head>
 <body>
 	<div class="wraper">
@@ -43,60 +46,175 @@
 			</form>
 		</div>
 
+		<div class="search__form">
+			<form action="#" class="search-order__form">
+
+				<label for="order-id1" class="order__input-label">
+					<span class="order__input-label">Поиск товара</span>
+					<div class="search-input__wrap">
+						<input type="text" required="required" class="order__input-text order__input-search">
+						<i class="search-ico"></i>
+					</div>
+				</label>
+
+				<input type="submit" class="send__order-btn btn" value="Добавить">
+			</form>
+		</div>
+
 
 		<div class="center">
-			<table class="grid-table">
-				<thead class="grid-table__row">
-					<td class="grid-table__column">Порядковый номер</td>
-					<td class="grid-table__column">Наименование товара</td>
-					<td class="grid-table__column">Характеристика товара</td>
-					<td class="grid-table__column">Поставщик</td>
-					<td class="grid-table__column">Контактное лицо</td>
-					<td class="grid-table__column">Контактный телефон</td>
-					<td class="grid-table__column">Сайт</td>
-					<td class="grid-table__column">Вес единицы,кг</td>
-					<td class="grid-table__column">Объем единицы,куб.м</td>
-					<td class="grid-table__column">Цена за ед. руб</td>
-					<td class="grid-table__column">Количество</td>
-					<td class="grid-table__column">Итого вес, кг</td>
-					<td class="grid-table__column">Итого объем куб.м</td>
-					<td class="grid-table__column">Сумма, руб</td>
+			<table class="grid-table tablesorter"  id="myTable">
+				<thead>
+					<tr class="grid-table__row">
+						<th class="grid-table__column">Порядковый номер</th>
+						<th class="grid-table__column">Наименование товара</th>
+						<th class="grid-table__column">Характеристика товара</th>
+						<th class="grid-table__column">Поставщик</th>
+						<th class="grid-table__column">Контактное лицо</th>
+						<th class="grid-table__column">Контактный телефон</th>
+						<th class="grid-table__column">Сайт</th>
+						<th class="grid-table__column">Вес единицы,кг</th>
+						<th class="grid-table__column">Объем единицы,куб.м</th>
+						<th class="grid-table__column">Цена за ед. руб</th>
+						<th class="grid-table__column">Количество</th>
+						<th class="grid-table__column">Итого вес, кг</th>
+						<th class="grid-table__column">Итого объем куб.м</th>
+						<th class="grid-table__column">Сумма, руб</th>
+					</tr>
 				</thead>
 
-				<tr class="grid-table__row">
-					<td class="grid-table__column">1</td>
-					<td class="grid-table__column">Простыня 1.5 спальная бязь отб ГОСТ</td>
-					<td class="grid-table__column">Простыня 1.5 спальная бязь, отб. ГОСТ, размер 150х215 см, плотность 142г/кв.м</td>
-					<td class="grid-table__column">ООО "текстиль"</td>
-					<td class="grid-table__column">Иван</td>
-					<td class="grid-table__column">8 800 555-35-35</td>
-					<td class="grid-table__column">textril.ru</td>
-					<td class="grid-table__column">0.250</td>
-					<td class="grid-table__column">0.0025</td>
-					<td class="grid-table__column">140.00</td>
-					<td class="grid-table__column"></td>
-					<td class="grid-table__column"></td>
-					<td class="grid-table__column"></td>
-					<td class="grid-table__column"></td>
-				</tr>
-				<tr class="grid-table__row">
-					<td class="grid-table__column">1</td>
-					<td class="grid-table__column">Простыня 1.5 спальная бязь отб ГОСТ</td>
-					<td class="grid-table__column">Простыня 1.5 спальная бязь, отб. ГОСТ, размер 150х215 см, плотность 142г/кв.м</td>
-					<td class="grid-table__column">ООО "текстиль"</td>
-					<td class="grid-table__column">Иван</td>
-					<td class="grid-table__column">8 800 555-35-35</td>
-					<td class="grid-table__column">textril.ru</td>
-					<td class="grid-table__column">0.250</td>
-					<td class="grid-table__column">0.0025</td>
-					<td class="grid-table__column">140.00</td>
-					<td class="grid-table__column"></td>
-					<td class="grid-table__column"></td>
-					<td class="grid-table__column"></td>
-					<td class="grid-table__column"></td>
-				</tr>
+				<tbody>
+					<tr class="grid-table__row">
+						<td class="grid-table__column">1</td>
+						<td class="grid-table__column">Простыня 1.5 спальная бязь отб ГОСТ</td>
+						<td class="grid-table__column">Простыня 1.5 спальная бязь, отб. ГОСТ, размер 150х215 см, плотность 142г/кв.м</td>
+						<td class="grid-table__column">ООО "текстиль"</td>
+						<td class="grid-table__column">Петр</td>
+						<td class="grid-table__column">8 800 555-35-35</td>
+						<td class="grid-table__column">textril.ru</td>
+						<td class="grid-table__column">0.250</td>
+						<td class="grid-table__column">0.0025</td>
+						<td class="grid-table__column">142.00</td>
+						<td class="grid-table__column"></td>
+						<td class="grid-table__column"></td>
+						<td class="grid-table__column"></td>
+						<td class="grid-table__column"></td>
+					</tr>
+					<tr class="grid-table__row">
+						<td class="grid-table__column">2</td>
+						<td class="grid-table__column">Простыня 1.5 спальная бязь отб ГОСТ</td>
+						<td class="grid-table__column">Простыня 1.5 спальная бязь, отб. ГОСТ, размер 150х215 см, плотность 142г/кв.м</td>
+						<td class="grid-table__column">ООО "текстиль"</td>
+						<td class="grid-table__column">Иван</td>
+						<td class="grid-table__column">8 800 555-35-35</td>
+						<td class="grid-table__column">textril.ru</td>
+						<td class="grid-table__column">0.2350</td>
+						<td class="grid-table__column">0.00325</td>
+						<td class="grid-table__column">140.00</td>
+						<td class="grid-table__column"></td>
+						<td class="grid-table__column"></td>
+						<td class="grid-table__column"></td>
+						<td class="grid-table__column"></td>
+					</tr>
+				</tbody>
 			</table>
+
+			<!-- <table id="myTable" class="tablesorter grid-table">
+				<thead>
+					<tr class="grid-table__row">
+						<th class="grid-table__column">Порядковый номер</th>
+						<th class="grid-table__column">Наименование товара</th>
+						<th class="grid-table__column">Характеристика товара</th>
+						<th class="grid-table__column">Поставщик</th>
+						<th class="grid-table__column">Контактное лицо</th>
+						<th class="grid-table__column">Контактный телефон</th>
+						<th class="grid-table__column">Сайт</th>
+						<th class="grid-table__column">Вес единицы,кг</th>
+						<th class="grid-table__column">Объем единицы,куб.м</th>
+						<th class="grid-table__column">Цена за ед. руб</th>
+						<th class="grid-table__column">Количество</th>
+						<th class="grid-table__column">Итого вес, кг</th>
+						<th class="grid-table__column">Итого объем куб.м</th>
+						<th class="grid-table__column">Сумма, руб</th>
+					</tr>
+				</thead>
+
+				<tbody>
+					<tr class="grid-table__row">
+						<td class="grid-table__column">1</td>
+						<td class="grid-table__column">Простыня 1.5 спальная бязь отб ГОСТ</td>
+						<td class="grid-table__column">Простыня 1.5 спальная бязь, отб. ГОСТ, размер 150х215 см, плотность 142г/кв.м</td>
+						<td class="grid-table__column">ООО "текстиль"</td>
+						<td class="grid-table__column">Иван</td>
+						<td class="grid-table__column">8 800 555-35-35</td>
+						<td class="grid-table__column">textril.ru</td>
+						<td class="grid-table__column">0.250</td>
+						<td class="grid-table__column">0.0025</td>
+						<td class="grid-table__column">140.00</td>
+						<td class="grid-table__column"></td>
+						<td class="grid-table__column"></td>
+						<td class="grid-table__column"></td>
+						<td class="grid-table__column"></td>
+					</tr>
+					<tr class="grid-table__row">
+						<td class="grid-table__column">1</td>
+						<td class="grid-table__column">Простыня 1.5 спальная бязь отб ГОСТ</td>
+						<td class="grid-table__column">Простыня 1.5 спальная бязь, отб. ГОСТ, размер 150х215 см, плотность 142г/кв.м</td>
+						<td class="grid-table__column">ООО "текстиль"</td>
+						<td class="grid-table__column">Иван</td>
+						<td class="grid-table__column">8 800 555-35-35</td>
+						<td class="grid-table__column">textril.ru</td>
+						<td class="grid-table__column">0.250</td>
+						<td class="grid-table__column">0.0025</td>
+						<td class="grid-table__column">140.00</td>
+						<td class="grid-table__column"></td>
+						<td class="grid-table__column"></td>
+						<td class="grid-table__column"></td>
+						<td class="grid-table__column"></td>
+					</tr>
+					<tr class="grid-table__row">
+						<td class="grid-table__column">1</td>
+						<td class="grid-table__column">Простыня 1.5 спальная бязь отб ГОСТ</td>
+						<td class="grid-table__column">Простыня 1.5 спальная бязь, отб. ГОСТ, размер 150х215 см, плотность 142г/кв.м</td>
+						<td class="grid-table__column">ООО "текстиль"</td>
+						<td class="grid-table__column">Иван</td>
+						<td class="grid-table__column">8 800 555-35-35</td>
+						<td class="grid-table__column">textril.ru</td>
+						<td class="grid-table__column">0.250</td>
+						<td class="grid-table__column">0.0025</td>
+						<td class="grid-table__column">140.00</td>
+						<td class="grid-table__column"></td>
+						<td class="grid-table__column"></td>
+						<td class="grid-table__column"></td>
+						<td class="grid-table__column"></td>
+					</tr>
+					<tr class="grid-table__row">
+						<td class="grid-table__column">1</td>
+						<td class="grid-table__column">Простыня 1.5 спальная бязь отб ГОСТ</td>
+						<td class="grid-table__column">Простыня 1.5 спальная бязь, отб. ГОСТ, размер 150х215 см, плотность 142г/кв.м</td>
+						<td class="grid-table__column">ООО "текстиль"</td>
+						<td class="grid-table__column">Иван</td>
+						<td class="grid-table__column">8 800 555-35-35</td>
+						<td class="grid-table__column">textril.ru</td>
+						<td class="grid-table__column">0.250</td>
+						<td class="grid-table__column">0.0025</td>
+						<td class="grid-table__column">140.00</td>
+						<td class="grid-table__column"></td>
+						<td class="grid-table__column"></td>
+						<td class="grid-table__column"></td>
+						<td class="grid-table__column"></td>
+					</tr>
+				</tbody>
+			</table> -->
 		</div>
 	</div>
+
+	<script>
+		$(document).ready(function(){
+			$("#myTable").tablesorter();
+
+			console.log('hello')
+		});
+	</script>
 </body>
 </html>
